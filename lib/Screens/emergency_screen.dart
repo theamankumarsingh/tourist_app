fa009bb96af3cc2e3f67887bf47b49e4426b559d
Aashray <aashray446@gmail.com>
2022-03-22 11:14:21 +0530

    Added few more buttons

--- lib/Screens/emergency_screen.dart
+++ lib/Screens/emergency_screen.dart
@@ -17,22 +17,59 @@ class _emergencyState extends State<emergency> {
         centerTitle: true,
         title: const Text("Emergency Contacts"),
       ),
-      body: ListView(
-        children: <Widget>[
+      body: ListView(children: [
+        list_item("Aashray", '9804403207'),
+        list_item('Aman', '9805441275')
+      ]),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              CupertinoIcons.doc_checkmark,
+              color: Colors.red,
+            ),
+            label: 'FIR',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                CupertinoIcons.text_bubble,
+                color: Colors.blue,
+              ),
+              label: 'Emergency Message')
+        ],
+      ),
+    );
+  }
+}
+
+class list_item extends StatelessWidget {
+  String name = "";
+  String number = "";
+  list_item(String name, String number) {
+    this.name = name;
+    this.number = number;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
           ListTile(
             leading: const CircleAvatar(
               backgroundColor: Colors.white,
               backgroundImage: AssetImage('Assets/avatar.png'),
             ),
-            title: const Text(
-              'John Judah',
+            title: Text(
+              this.name,
             ),
-            subtitle: const Text('9804403207'),
+            subtitle: Text(this.number),
             trailing: ElevatedButton(
-                onPressed: () async {
-                  await FlutterPhoneDirectCaller.callNumber("+9779804403207");
-                },
-                child: Icon(CupertinoIcons.phone)),
+              onPressed: () async {
+                await FlutterPhoneDirectCaller.callNumber(this.number);
+              },
+              child: Icon(CupertinoIcons.phone),
+            ),
           ),
           const Divider(
             height: 1.0,
